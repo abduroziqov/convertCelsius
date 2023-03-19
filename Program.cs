b2a484dbe73a013f936f6532098a13502108f60d
abdurozikov.kuvonchbek@gmail.com <abdurozikov.kuvonchbek@gmail.com>
2023-03-19 22:17:06 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace convertCelsius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the amount of Celsius: ");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kelvin = {0}", celsius + 273);
+            Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32);
+        }
+    }
+}
+
